f2d3028932024c6a263907a93f752324f5e13ff6
sam <145283692+samsumer01@users.noreply.github.com>
2025-01-26 10:12:52 +0000

    datattypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version 
+
+//alert(3+4) //we are using nodejs,not brower
+
+console.log(3+3)
+console.log('sam')
+
+
+let name="sam"
+let age=18
+let isLoggedIn=false
+
+//number => 2 to power 53
+//bigint
+//string =>""
+//boolean=>true/false
+//null 
+//undefined
+//symbol=> unique
+//oject
+
+console.log(typeof null)
+console.log(typeof undefined)
