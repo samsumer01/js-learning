8c2f902abd7ef5932b1061691419bcc16be01e63
sam <145283692+samsumer01@users.noreply.github.com>
2025-01-26 12:25:46 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,33 @@
+// premitive 
+
+// 7 types : string , Number, Boolean , null, undefined,Symbol,BigInt
+
+const score=100
+const scoreValue=100.5
+
+const isLoggedIN = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id ===anotherId);
+
+const bigNumber = 2423423242342342423n
+
+//  Reference ( Non premitive)
+// Array, Oject , funtion 
+
+const heros = ["sam","erath","soil"]
+let myObj={
+    name:"sam",
+    age:22,
+}
+
+
+const myFunction = function(){
+    console.log("Hello world")
+}
+
+console.log( typeof outsideTemp);
