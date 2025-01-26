f4517092412b0a114b5ec52c61b4bf9df5cf96de
sam <145283692+samsumer01@users.noreply.github.com>
2025-01-26 10:27:53 +0000

    conversion of datatypes

---
+++ 01_basics/03_coversionOperation.js
@@ -0,0 +1,26 @@
+let score=false
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33"=>33
+//"33abc"=>Nan
+//true =>1; false=>0
+
+let isLoggedIn="sam"
+let booleanisLoggedIn=Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+// =>true ;0 =>false
+//""=>false
+//"sam"=>true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
